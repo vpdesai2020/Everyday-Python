1e9996c3bb03a4b6ca9490ed5f8ebcfde41f871b
Vasant Kumar Desai <33997408+vpdesai2020@users.noreply.github.com>
2020-05-29 20:29:03 +0530

    Update shutdown_the_system.py

--- Shutdown the system/shutdown_the_system.py
+++ Shutdown the system/shutdown_the_system.py
@@ -1,21 +1,19 @@
 import os 
 import platform
   
-Ask = input("Would you like to shutdown your computer ? (yes / no): ") 
-  
-if Ask == 'no': 
-    print("Okay :)")
-    exit() 
-else: 
-    shutdown()
-    
-    
+   
 def shutdown():
-    import os
-    import platform
- 
-    if platform.system()=="Windows":
-        os.system("shutdown -s -t 0")
- 
-    else:
-        os.system("shutdown -h now")
+
+    Ask = input("Would you like to shutdown your computer ? (yes / no):   ") 
+  
+    if Ask == 'no': 
+        print("Okay chill :)")
+        exit() 
+    else: 
+        if platform.system()=="Windows":
+            os.system("shutdown -s -t 0")
+     
+        else:
+            os.system("shutdown -h now")
+
+shutdown()
