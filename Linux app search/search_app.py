9bcba94efe2740e08681255d3b59003cb8d9c42c
Vasant Kumar Desai <33997408+vpdesai2020@users.noreply.github.com>
2020-06-17 14:48:44 +0530

    Add files via upload

---
+++ Linux app search/search_app.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import subprocess
+
+window = Tk()
+
+window.title("Linux App Search Box")
+window.geometry('300x100')
+
+lbl = Label(window, text="Search Application,.")
+lbl.grid(column=2, row=3)
+
+txt = Entry(window,width=10)
+txt.grid(column=2, row=0)
+
+def clicked():
+    lbl.configure(text="Searching,..")
+    check()
+
+btn = Button(window, text="Search", command=clicked)
+btn.grid(column=3, row=0)
+
+def check():
+    if len(txt.get()) == 0:
+        lbl.configure(text="Please enter,..")
+    else:
+        f()
+
+def f():
+    try:
+        null = open("/dev/null", "w")
+        subprocess.Popen(txt.get(), stdout=null, stderr=null)
+        res = txt.get() + " application found"
+        lbl.configure(text=res)
+        null.close()
+    except OSError:
+        res = txt.get() + " application NOT found"
+        lbl.configure(text=res)
+window.mainloop()
